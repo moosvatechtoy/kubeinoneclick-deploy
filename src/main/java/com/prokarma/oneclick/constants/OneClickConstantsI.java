6c03b57c34188c90d160ae64c900dd959cc968d1
Ramesh Reddy Musireke <mramesh@pkglobal.com>
2020-07-22 11:37:19 +0530

    Missing constants/OneClickConstantsI.java file

---
+++ src/main/java/com/prokarma/oneclick/constants/OneClickConstantsI.java
@@ -0,0 +1,7 @@
+package com.prokarma.oneclick.constants;
+
+public interface OneClickConstantsI {
+    public static final String GOOGLE_CREDENTIALS_PATH="/usr/local/credentials.json";
+    public static final String GOOGLE_PROVIDER = "GOOGLE";
+    public static final String CRED_VAR_KEY = "credentials";
+}
